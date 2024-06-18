3814dec007c7695b3f6e8e824021b8d7d864f3ff
SHAIKARSHADA <shaikarshad1792004@gmail.com>
2024-06-18 21:50:17 +0530

    added dom practice

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function calculateSum() {
+  const x = document.getElementById("one").value;
+  const y = document.getElementById("two").value;
+  const element = document.getElementById("finalSum")
+  element.innerHTML = parseInt(x) + parseInt(y);
+
+ 
+}
+
