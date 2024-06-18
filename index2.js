eab1abc43f98a92fcfb80b5f96f8aa6bce5c4574
SHAIKARSHADA <shaikarshad1792004@gmail.com>
2024-06-18 22:30:37 +0530

    added index.js

---
+++ index2.js
@@ -0,0 +1,7 @@
+async function calculateSum() {
+  const p = document.getElementById("principal").value;
+  const r = document.getElementById("rate").value;
+  const t = document.getElementById("time").value;
+  
+  
+}
