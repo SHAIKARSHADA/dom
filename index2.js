7ec6ba8ee78ef8acadaf754441cd33f8fcce457c
SHAIKARSHADA <shaikarshad1792004@gmail.com>
2024-06-18 22:34:26 +0530

    almost completed

--- index2.js
+++ index2.js
@@ -2,6 +2,11 @@ async function calculateSum() {
   const p = document.getElementById("principal").value;
   const r = document.getElementById("rate").value;
   const t = document.getElementById("time").value;
-  
+
+
+  const res = await fetch("https://sum-server.100xdevs.com/interest?pricinpal="+p+"&rate="+r+"&time="+t);
+  const ans = res.text();
+
+  document.getElementById("finalSum").innerHTML = ans;
   
 }
