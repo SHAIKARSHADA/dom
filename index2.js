43cc1a078670453dc29c498da32c5a9aac05f80a
SHAIKARSHADA <shaikarshad1792004@gmail.com>
2024-06-18 22:49:14 +0530

    corrected some errors

--- index2.js
+++ index2.js
@@ -1,12 +1,13 @@
-async function calculateSum() {
+async function calculatePRI() {
   const p = document.getElementById("principal").value;
   const r = document.getElementById("rate").value;
   const t = document.getElementById("time").value;
 
 
-  const res = await fetch("https://sum-server.100xdevs.com/interest?pricinpal="+p+"&rate="+r+"&time="+t);
-  const ans = res.text();
-
-  document.getElementById("finalSum").innerHTML = ans;
+  const res = await fetch("https://sum-server.100xdevs.com/interest?principal="+p+"&rate="+r+"&time="+t);
+  console.log(res);
+  const ans = await res.json();
+  document.getElementById("finalSum").innerText= "Total: "+ ans.total;
+  document.getElementById("finalSum2").innerText= "Total: "+ ans.rate;
   
 }
