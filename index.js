c3a07c4e84ac8c54c8f589e170000a697b7efce1
SHAIKARSHADA <shaikarshad1792004@gmail.com>
2024-06-18 22:21:25 +0530

    we did async and await with dom

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
-function calculateSum() {
-  const x = document.getElementById("one").value;
-  const y = document.getElementById("two").value;
+async function calculateSum() {
+  const a = document.getElementById("one").value;
+  const b = document.getElementById("two").value;
   const element = document.getElementById("finalSum")
-  element.innerHTML = parseInt(x) + parseInt(y);
-
- 
+  const res = await fetch("https://sum-server.100xdevs.com/sum?a="+a+"&b="+b); 
+  const ans = await res.text();
+  document.getElementById("finalSum").innerHTML = ans;
 }
 
